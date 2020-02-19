b8fe5b001eacfd31f1faacdd051cc6a3773d6c9e
yourname <youremail>
2020-02-19 12:50:51 -0500

    2

--- example/main.cpp
+++ example/main.cpp
@@ -37,27 +37,26 @@ bool judge(const vector<float> &neck, const vector<float> &rhip, const vector<fl
     return true;
 }
 
-cv::Mat pose(std::vector<HumanPose> poses, cv::Mat frame){
-    vector<vector<float>> keypoint;
+cv::Mat fall(std::vector<HumanPose> poses, cv::Mat frame){
+    vector<cv::Point2f> keypoint;
+	int body[3][2];
+	RotatedRect rect;
     for (HumanPose const& pose : poses) {
-        std::stringstream rawPose;
-        rawPose << std::fixed << std::setprecision(0);
-        rawPose << pose.score;
-        //std::cout<<"pose.keypoints"<<pose.keypoints<<endl;
-        keypoint = {{pose.keypoints[1].x , pose.keypoints[1].y}, {pose.keypoints[8].x, pose.keypoints[8].y}, {pose.keypoints[11].x, pose.keypoints[11].y}};
-        float x_max = 0.0 , y_max = 0.0 , x_min = 1280.0, y_min = 720.0;
+		for(cv::Point2f point : pose.keypoints){
+			if(int(point.x) == -1){continue;}
+			keypoint.push_back(point);
+		}
+        body = {{pose.keypoints[1].x , pose.keypoints[1].y}, {pose.keypoints[8].x, pose.keypoints[8].y}, {pose.keypoints[11].x, pose.keypoints[11].y}};
 
-        for(cv::Point2f point : pose.keypoints){
-            if(point.x > x_max){x_max = point.x;}
-            if(point.x < x_min){x_min = point.x;}
-            if(point.y > y_max){y_max = point.y;}
-            if(point.y < y_min){y_min = point.y;}
-        }
         if(judge(keypoint[0],keypoint[1],keypoint[2])){
             //cout<<"falling"<<endl;
-            cv::rectangle(frame, {int(x_min), int(y_min)}, {int(x_max), int(y_max)}, cv::Scalar(0, 0, 255), 2);
+
+		rect = cv::minAreaRect(keypoint);
+		cv::rectangle(frame, rect.boundingRect(), cv::Scalar(0, 0, 255), 2);
+        std::cout<<"keypoint"<<keypoint<<endl;
             cv::putText(frame,"falling",cv::Point(x_min+7,y_min-4), cv::FONT_HERSHEY_SIMPLEX, 0.6,cv::Scalar(255,255,255),1,8);
         }
+
     }
     return frame;
 }
@@ -66,14 +65,14 @@ int main(int argc, const char** argv){
 
     HumanPoseEstimator estimator("model/human_pose_light_model.pt");
 
-    deque<int> mem(10);
+    //deque<int> mem(10);
     cv::namedWindow("result",cv::WINDOW_NORMAL);
 
     std::string model_file = "model/model_head.engine";
     Object_Detection::Object_detect net(model_file);
     //std::unique_ptr<float[]> outputData(new float[net.outputBufferSize]);
     cv::Mat frame;
-    cv::VideoCapture cap("fall/1.avi");
+    cv::VideoCapture cap("fall/2.avi");
 //    cv::VideoCapture cap(0);
     cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
     cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
@@ -83,35 +82,39 @@ int main(int argc, const char** argv){
     cv::VideoWriter outputVideo;
     cv::Size s = cv::Size((int)cap.get(CV_CAP_PROP_FRAME_WIDTH),
                           (int)cap.get(CV_CAP_PROP_FRAME_HEIGHT));
-    outputVideo.open("head.avi", CV_FOURCC('X','V','I','D'), 25.0,
+    outputVideo.open("g_video/head.avi", CV_FOURCC('X','V','I','D'), 25.0,
                      s, true);
     if (!outputVideo.isOpened())
     {
         cout << "Open video error !"<<endl;
         return -1;
     }
-	cv::Mat frame2;
+
     while (cap.read(frame))
     {
         int start = cv::getTickCount();
 //        pose estimation code
         std::vector<HumanPose> poses = estimator.estimate(frame);
-        frame2 = pose(poses, frame);
-
 //        human detection
         net.detect(frame, Obj_pool);
         Result result;
 //        draw img
-        renderHumanPose(poses, frame2);
-        Object_Detection::getResult(result, frame2, Obj_pool);
+	// draw falling
+        frame = fall(poses, frame);
+	// draw pose
+        renderHumanPose(poses, frame);
+	//draw head
+        Object_Detection::getResult(result, frame, Obj_pool);
+
         std::cout<<"status:  "<<result.first<<"   numbers:  "<<result.second<<std::endl;
-        cv::putText(frame2, result.first, cv::Point(900, 50),cv::FONT_HERSHEY_SIMPLEX,2,cv::Scalar(0,0,255),4,8);
-        cv::putText(frame2, "number: "+to_string(result.second), cv::Point(0, 200),cv::FONT_HERSHEY_SIMPLEX,2,cv::Scalar(0,0,255),4,8);
+        cv::putText(frame, result.first, cv::Point(900, 50),cv::FONT_HERSHEY_SIMPLEX,2,cv::Scalar(0,0,255),4,8);
+        cv::putText(frame, "number: "+to_string(result.second), cv::Point(0, 200),cv::FONT_HERSHEY_SIMPLEX,2,cv::Scalar(0,0,255),4,8);
+
 //        output video
         int end_tick = cv::getTickCount();
         std::cout << " detect time:" << 1000.0 * (end_tick - start) / cv::getTickFrequency() << "ms" << std::endl << std::endl;
-        cv::imshow("result",frame2);
-        outputVideo<< frame2;
+        cv::imshow("result",frame);
+        outputVideo<< frame;
 //        waitkey
         if(cv::waitKey(1) == 'q'){
             cv::destroyAllWindows();
